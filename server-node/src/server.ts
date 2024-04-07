cd7be1103945b1d2f58d23bd58641c3289a4f8ea
Michel <michel.geronimo@hotmail.com>
2024-04-07 11:43:45 -0300

    Adicionando rotas e configuração de api

--- server-node/src/server.ts
+++ server-node/src/server.ts
@@ -1,40 +1,20 @@
-import { create } from "domain";
 import fastify from "fastify";
-import { z } from 'zod';
-import { PrismaClient } from '@prisma/client';
+import { serializerCompiler, validatorCompiler, jsonSchemaTransform, ZodTypeProvider } from 'fastify-type-provider-zod'
+import { createEvent } from "./routes/create-event";
+import { registerForEvent } from "./routes/register-for-event";
+import { getEvent } from "./routes/get-event";
+import { getAttendeeBadge } from "./routes/get-attendee-badge";
 
 
 const app = fastify()
 
-const prisma = new PrismaClient({
-    log: ['query'],
-})
-//Métodos HTTP: GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS, ...
-//Corpo da requisição
-//Parametros de busca
-//Parametros de rota
-//Cabeçalhos
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-app.post('/events', async (request, reply) => {
-    const createEventSchema = z.object({
-        title: z.string().min(4),
-        details: z.string().nullable(),
-        maximumAttenddes: z.number().int().positive().nullable(),
-    })
-
-    const data = createEventSchema.parse(request.body)
-
-    const event = await prisma.event.create({
-        data: {
-            title: data.title,
-            details: data.details,
-            maximumAttendees: data.maximumAttenddes,
-            slug: new Date().toISOString(),
-        }
-    })
-
-    return reply.status(201).send({ eventId: event.id })
-})
+app.register(createEvent)
+app.register(registerForEvent)
+app.register(getEvent)
+app.register(getAttendeeBadge)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running!')
